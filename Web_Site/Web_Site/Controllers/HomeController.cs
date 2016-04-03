44c1af8cf6911f1b23489283bbce779a14d183fb
Antoxa <antoxadoanse@mail.ru>
2016-04-03 22:42:57 +0400

    [*]Добавлены модели и контроллер[1]

--- Web_Site/Web_Site/Controllers/HomeController.cs
+++ Web_Site/Web_Site/Controllers/HomeController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_Site.Models;
+
 
 namespace Web_Site.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+		// создаем контекст данных
+		ContextUser db = new ContextUser();
+		public ActionResult Index()
         {
-            return View();
+			// получаем из бд все объекты 
+			IEnumerable<User> users = db.Users;
+			return View();
         }
-       
-    }
+
+		/// <summary>
+		/// Добавление
+		/// </summary>
+		/// <returns></returns>
+
+		[HttpGet]
+		public ActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Create(User user)
+		{
+			db.Users.Add(user);
+			db.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+
+
+	}
 }
