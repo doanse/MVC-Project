aea4e7c520199ad42525fb79b65751983de7ccf4
Antoxa <antoxadoanse@mail.ru>
2016-04-08 01:28:16 +0400

    [+]Identity минимум для регистрации[2]

---
+++ MVC_Project/MVC_Project/App_Start/Startup.Auth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using MVC_Project.Models;
+
+namespace MVC_Project
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+			// настраиваем контекст и менеджер
+			app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+			app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+			app.UseCookieAuthentication(new CookieAuthenticationOptions
+			{
+				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+				LoginPath = new PathString("/Account/Login"),
+			});
+		}
+    }
+}
---
+++ MVC_Project/MVC_Project/App_Start/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using MVC_Project.Models;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project.App_Start
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			// настраиваем контекст и менеджер
+			app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+			app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+			app.UseCookieAuthentication(new CookieAuthenticationOptions
+			{
+				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+				LoginPath = new PathString("/Account/Login"),
+			});
+		}
+	}
+}
